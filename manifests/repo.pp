c41c18faf120c88035da31411ad7e8bbc59e30bb
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-08-14 13:32:22 +0000

    Cambio il nome del modulo utilizzato per la migrazione

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,13 +1,13 @@
 class mysql::repo
 {
-  include apt_puppetlabs
+  include apt
 
   case $mysql::type{
     'oracle' : { # use standard packages
     }
 
     'percona': {
-      apt_puppetlabs::source {'percona':
+      apt::source {'percona':
         location    => 'http://repo.percona.com/apt',
         repos       => 'main',
         key         => 'CD2EFD2A',
@@ -16,7 +16,7 @@ class mysql::repo
     }
 
     'mariadb': {
-      apt_puppetlabs::source {'mariadb5.3':
+      apt::source {'mariadb5.3':
         location    => 'http://mirror2.hs-esslingen.de/mariadb/repo/5.3/debian',
         repos       => 'main',
         key         => '1BB943DB',
