6b0ac4801fbb995d53c7294613024ea7ff8acc16
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-08-14 07:38:41 +0000

    adattato al nuovo modulo apt

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,32 +1,26 @@
 class mysql::repo
 {
-  include apt
+  include apt_puppetlabs
 
   case $mysql::type{
     'oracle' : { # use standard packages
     }
 
     'percona': {
-      apt::key {
-        'CD2EFD2A':
-      }
-
-      apt::sources_list {"percona":
-          ensure  => present,
-          content => "deb http://repo.percona.com/apt $lsbdistcodename main",
-          require => Apt::Key['CD2EFD2A'],
+      apt_puppetlabs::source {'percona':
+        location    => 'http://repo.percona.com/apt',
+        repos       => 'main',
+        key         => 'CD2EFD2A',
+        include_src => false,
       }
     }
 
-    'mariadb': { 
-      apt::key {
-        '1BB943DB':
-      }
-
-      apt::sources_list {"mariadb5.3":
-          ensure  => present,
-          content => "deb http://mirror2.hs-esslingen.de/mariadb/repo/5.3/debian $lsbdistcodename main",
-          require => Apt::Key['1BB943DB'],
+    'mariadb': {
+      apt_puppetlabs::source {'mariadb5.3':
+        location    => 'http://mirror2.hs-esslingen.de/mariadb/repo/5.3/debian',
+        repos       => 'main',
+        key         => '1BB943DB',
+        include_src => false,
       }
     }
   }
